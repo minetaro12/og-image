135f65400c4779fb7a3fa894ede21b256bd844d5
minetaro12 <osawa2003@gmail.com>
2022-02-22 21:40:17 +0900

    fix parser

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -5,7 +5,7 @@ import { ParsedRequest } from './types';
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
     const { pathname, query } = parse(req.url || '/', true);
-    const { fontSize, theme, md, background } = (query || {});
+    const { fontSize, textcolor, md, background } = (query || {});
 
     if (Array.isArray(fontSize)) {
         throw new Error('Expected a single fontSize');
